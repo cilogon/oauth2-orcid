34afb041393dc01221e356fdf67295237c9c832e
Terry Fleury <terrencegf@gmail.com>
2017-05-25 17:49:32 -0500

    Change assertRegExp to assertEquals.

--- test/src/Provider/ORCIDTest.php
+++ test/src/Provider/ORCIDTest.php
@@ -75,7 +75,7 @@ class ORCIDTest extends \PHPUnit_Framework_TestCase
         $token->shouldReceive('getResourceOwnerId')->andReturn($id);
         $url = $this->provider->getResourceOwnerDetailsUrl($token);
         $uri = parse_url($url);
-        $this->assertRegExp("/\/v2.0\/$id\/record/", $uri['path']);
+        $this->assertEquals("/v2.0/$id/record", $uri['path']);
     }
 
     public function testGetAccessToken()
